d952d823bd8a8ac670d3d3b4a15b4d28fab405a4
employeemanagement13-glitch <employeemanagement13@gmail.com>
2026-09-12 12:11:37 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "MDOS Portal",
+  title: "Distroline - Your Distribution Management System",
   description: "Distribution Management System",
 };
 
